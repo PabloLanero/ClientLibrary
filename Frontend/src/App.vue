0c39486b495e258a3325320f18629a8632c85b31
Pablo Lanero <pablolaneroperez@gmail.com>
2026-02-03 15:37:00 +0100

    Añadido vuetify

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -6,6 +6,9 @@
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
+  <v-btn>
+    Ejemplo
+  </v-btn>
 </template>
 
 <style scoped></style>
